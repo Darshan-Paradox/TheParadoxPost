d41b2eb0f5144499bb39e441711567e9fb2e1126
Darshan-Paradox <darshan.paradox@gmail.com>
2023-10-28 01:32:23 +0530

    Modified Blog dynamic import

--- client/src/routes/Blog.svelte
+++ client/src/routes/Blog.svelte
@@ -1,15 +1,18 @@
 <script>
+	import { onMount } from "svelte";
+
 	export let params;
+
 	async function get_blog()
 	{
 		const component = (await import(`../blogs/${params.cat}/${params.id}.svelte.md`)).default;
 		return component;
 	}
 
-	let blog = get_blog();
+	let blog = null;
+
+	onMount(async () => {blog = await get_blog()})
 </script>
 
-{#await blog then content}
-	<svelte:component this={content}>
-	</svelte:component>
-{/await}
+<svelte:component this={blog}>
+</svelte:component>
